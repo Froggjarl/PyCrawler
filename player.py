84bd4797c78373e1a5c30623a05f64717aa17ccc
Froggjarl <liamh.mws@gmail.com>
2026-08-19 13:50:47 -0400

    Added player attack function

--- player.py
+++ player.py
@@ -1,5 +1,6 @@
+import random
 
-def create_player(name='Hero'):
+def create_player(name):
 
     if name == '' or name == ' ':
         name = 'Hero'
@@ -14,4 +15,18 @@ def create_player(name='Hero'):
         'crit_mult': 2
     }
     return player
+
+def player_attack(player):
+
+    player_damage = random.randint(player['min_dmg'], player['max_dmg'])
+    crit_roll = random.randint(1, 100)
+
+    if crit_roll <= player['crit_chance']:
+        player_damage *= player['crit_mult']
+        print(f"Critical hit! {player['name']} dealt {player_damage} damage!")
+
+    else:
+        print(f"{player['name']} dealt {player_damage} damage.")
+
+    return player_damage
 
