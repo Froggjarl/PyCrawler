789bf7985ee115e1a4231379bea1df2d6c52c41e
Froggjarl <liamh.mws@gmail.com>
2026-08-20 21:09:26 -0400

    Added magic points

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from player import player_fireball
 from enemies import create_ran_enemy
 from enemies import enemy_attack
 from enemies import enemy_types
+from print_graphics import print_enemy
 
 score = 0
+mp = 3 #Mana points
 
 print("""
 ██████╗ ██╗   ██╗ ██████╗██████╗  █████╗ ██╗    ██╗██╗     ███████╗██████╗ 
@@ -28,39 +30,47 @@ while player['current_hp'] > 0:
 
     print(f"{player['name']} encountered a {enemy['name']}!")
 
+    print_enemy(enemy)
+
     while player['current_hp'] > 0 and enemy_defeated == False:
+        print(f'\nMP: {mp}')
         print(f"What will {player['name']} do?\n[1] Attack\n[2] Fireball Spell")
         player_act = input(': ')
 
         if player_act == '' or player_act == ' ':
-            print("You can't do nothing!")
+            print("\nYou can't do nothing!")
             continue
 
         if player_act == '1':
             player_damage = player_attack(player)
             enemy['current_hp'] -= player_damage
-            print(f"{player['name']} dealt {player_damage} damage to {enemy['name']}! {enemy['name']} has {enemy['current_hp']} HP left.")
+            print(f"\n{player['name']} dealt {player_damage} damage to {enemy['name']}! {enemy['name']} has {enemy['current_hp']} HP left.")
 
         if player_act == '2':
-            player_damage = player_fireball(player)
-            enemy['current_hp'] -= player_damage
-            print(f"{player['name']} cast Fireball and dealt {player_damage} damage to {enemy['name']}! {enemy['name']} has {enemy['current_hp']} HP left.")
+            if mp <= 0:
+                print("\nYou don't have enough mana to cast Fireball!")
+                continue
+            else:
+                mp -= 1
+                player_damage = player_fireball(player)
+                enemy['current_hp'] -= player_damage
+                print(f"\n{player['name']} cast Fireball and dealt {player_damage} damage to {enemy['name']}! {enemy['name']} has {enemy['current_hp']} HP left.")
 
         if enemy['current_hp'] <= 0:
-            print(f"{enemy['name']} has been defeated!")
+            print(f"\n{enemy['name']} has been defeated!")
             score += 1
             enemy_defeated = True
             break
 
         enemy_damage = enemy_attack(enemy)
         player['current_hp'] -= enemy_damage
-        print(f"{enemy['name']} dealt {enemy_damage} damage to {player['name']}! {player['name']} has {player['current_hp']} HP left.")
+        print(f"\n{enemy['name']} dealt {enemy_damage} damage to {player['name']}! {player['name']} has {player['current_hp']} HP left.")
 
         if player['current_hp'] <= 0:
             break
 
 if player['current_hp'] <= 0:
-        print(f"{player['name']} has been defeated by {enemy['name']}! Game Over.")
+        print(f"\n{player['name']} has been defeated by {enemy['name']}! Game Over.")
         print(f"Final score: {score}")
         
 
