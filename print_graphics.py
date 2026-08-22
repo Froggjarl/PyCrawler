68424acd35dcf1d0ecbd9dc037e42edf6e8d4210
Froggjarl <liamh.mws@gmail.com>
2026-08-22 12:36:25 -0400

    Forced UTF-8 encoding

--- print_graphics.py
+++ print_graphics.py
@@ -6,14 +6,14 @@ slime_path = Path(__file__).parent / "assets" / "enemies" / "slime.txt"
 
 def print_enemy(enemy):
     if enemy['name'] == 'Goblin':
-        with open(gob_path, 'r') as gob:
+        with open(gob_path, 'r', encoding='utf-8') as gob:
             print(gob.read())
 
     elif enemy['name'] == 'Slime':
-        with open(slime_path, 'r') as slime:
+        with open(slime_path, 'r', encoding='utf-8') as slime:
             print(slime.read())
 
     elif enemy['name'] == 'Wizard':
-        with open(wiz_path, 'r') as wiz:
+        with open(wiz_path, 'r', encoding='utf-8') as wiz:
             print(wiz.read())
 
